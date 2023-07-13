aada471db61e1a013a3409e276f013304786e1ea
luzhuang <364439895@qq.com>
2023-07-13 16:23:13 +0800

    feat: add git-lfs

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ export default class Example extends React.Component {
     return (
       <div className="x6-graph-wrap">
         <div className="x6-graph">test</div>
-        <canvas id="canvas" style={{background: "blue"}}></canvas>
+        <canvas id="canvas" style={{background: "blue", width: "100vw", height: "100vh"}}></canvas>
       </div>
     )
   }
